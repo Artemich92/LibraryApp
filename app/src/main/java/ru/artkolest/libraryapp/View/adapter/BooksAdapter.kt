8ca9d964c951484d4504e304e8cfd84525c519f5
artem <artem.kolesnichenko.1992@mail.ru>
2021-09-15 15:59:11 +0300

    Implemented animation for RecyclerView

--- app/src/main/java/ru/artkolest/libraryapp/View/adapter/BooksAdapter.kt
+++ app/src/main/java/ru/artkolest/libraryapp/View/adapter/BooksAdapter.kt
@@ -1,19 +1,24 @@
 package ru.artkolest.libraryapp.View.adapter
 
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
 import androidx.paging.PagingDataAdapter
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import ru.artkolest.booksapp.data.entities.Book
+import ru.artkolest.libraryapp.R
 
 class BooksAdapter : PagingDataAdapter<Book, RecyclerView.ViewHolder>(REPO_COMPARATOR) {
 
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         val news = getItem(position)
+        holder.itemView.animation =
+            AnimationUtils.loadAnimation (holder.itemView. context , R.anim.anim_resourse_file)
         if(news != null){
             (holder as BooksViewHolder).bind(news)
         }
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
